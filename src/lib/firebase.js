328807d183f3098a75aebb9b8d8d7cfbe69e525e
Shawn McPeek <shawnmcpeek@gmail.com>
2024-12-14 13:15:31 -0700

    secrets

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,15 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDf9Fp2a_84eHdeimmsAst4r6CkODMGQvU",
-	authDomain: "coaster-score.firebaseapp.com",
-	projectId: "coaster-score",
-	storageBucket: "coaster-score.firebasestorage.app",
-	messagingSenderId: "953948340409",
-	appId: "1:953948340409:web:11707a08f997471de59b68",
-	measurementId: "G-E6862R9FJK"
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
